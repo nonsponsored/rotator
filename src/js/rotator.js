44cbd1d256e23c0502c53047f65e8f47c3971cc7
nonsponsored <gh@nonsponsored.com>
2018-02-21 10:10:44 -0600

    adding comments to js

--- src/js/rotator.js
+++ src/js/rotator.js
@@ -9,26 +9,26 @@ var manageRotator = function(trigger) {
 			// clone first picture 
 			var cloneFirstPicture = $rotatorSlide[0].querySelectorAll('picture')[0].cloneNode(true);
 			$rotator.appendChild(cloneFirstPicture);
-	
+			
+			// set rotator to active
 			$rotator.classList.add('js-rotator--active');
 			
+			// create bullets
 			var i,
 				$rotatorBullets = document.createElement('ul');
-
 			$rotatorBullets.className = 'rotator__bullets';
 			$rotator.appendChild($rotatorBullets);
-
 			for (i = 1; i <= $rotatorSlideCount; i++) {
 				var li = document.createElement('li');
 				$rotatorBullets.appendChild(li);
 			}
-		
 			var $rotatorBulletList = $rotator.querySelector('.rotator__bullets'),
 				$rotatorBullet = $rotatorBulletList.querySelectorAll('li');
 			$rotatorBullet[0].classList.add('rotator__bullet--active');
 			
+			// add click event
 			$rotatorBulletList.addEventListener('click', function (e) {
-				if(e.target && e.target.nodeName == "LI") { // could use e.target.matches('LI' if don't need IE11 support
+				if(e.target && e.target.nodeName === 'LI') { // could use e.target.matches('LI' if don't need IE11 support
 					// get index of li
 					var index = Array.prototype.indexOf.call(this.children, e.target);
 					// remove current slide class
@@ -42,7 +42,7 @@ var manageRotator = function(trigger) {
 				        bullet.classList.remove('rotator__bullet--active');
 				    });
 					// set active bullet class
-					//e.target.classList.add('rotator__bullet--active');
+					// alternate option to set active bullet: e.target.classList.add('rotator__bullet--active');
 					$rotatorBullet[index].classList.add('rotator__bullet--active');
 				}
 			}, false);
